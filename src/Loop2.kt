9ac99c981783ced5bae7a0b5ea8cf46b872abc40
alincqeek <a72658695@gmail.com>
2025-09-27 10:27:46 +0300

    Initial commit

---
+++ src/Loop2.kt
@@ -0,0 +1,25 @@
+fun main() {
+    // Шаг 1. Цикл while
+    /*while (условие) {
+        //Код который будет повторяться
+    }*/
+    /*var number = 1
+    while (number <= 5) {
+        println("Число: $number")
+        number++
+    }*/
+
+    // Шаг 2. Операторы continue и break
+
+    /*var number = 0
+    while (number < 10) {
+        number++
+        if (number == 3) {
+            println("Останавливаемся на $number")
+            continue
+        }
+        println(number)
+    }*/
+
+
+}
