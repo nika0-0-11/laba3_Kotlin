8d3b5209e993eacd9cab02ab0ecd8742430c7b8e
alincqeek <a72658695@gmail.com>
2025-09-27 11:32:38 +0300

    leee

---
+++ src/Random.kt
@@ -0,0 +1,22 @@
+import kotlin.random.Random
+
+fun main() {
+    //генерирует целое значение между int.min_val and int.val_vax
+    /*println(Random.nextInt())*/
+
+    /*println(Random.nextLong())
+    println(Random.nextFloat())
+    println(Random.nextDouble())*/
+
+    /*Random.nextInt(100)
+    Random.nextInt(1, 100)
+    Random.nextLong(100)
+    Random.nextLong(1,100)
+    Random.nextDouble(5.0)
+    Random.nextDouble(0.0,5.0)*/
+
+    /*val target = Random.nextInt(1,100)
+    println("Угадайте число от 1 до 100")*/
+
+}
+
